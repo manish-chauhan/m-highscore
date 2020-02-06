180bf92009ec5eb149f8832dc3caa87cfd1faf98
Manish Chauhan <chauhan@Manishs-MacBook-Air.local>
2020-02-06 21:28:11 +0530

    Internal server writte

--- internal/server/grpc/grpc.go
+++ internal/server/grpc/grpc.go
@@ -11,6 +11,13 @@ type Grpc struct {
 
 var HighScore  = 999999999.0
 
+
+func NewServer(address string) *Grpc {
+	return &Grpc{
+	address: address,
+	}
+}
+
 func (g *Grpc) SetHighScore(ctx context.Context, input *pbhighscore.SetHighScoreRequest) (*pbhighscore.SetHighScoreResponse , error){
 	log.Info().Msg("SetHighScore in m-highscore is called")
 	HighScore = input.HighScore
@@ -27,3 +34,23 @@ func (g *Grpc) GetHighScore(ctx context.Context, input *pbhighscore.GetHighScore
 	}, nil
 }
 
+func (g *Grpc) ListenAndServe() error {
+	lis, err := net.Listen("tcp", g.address)
+	if err != nil {
+		return errors.Wrap(err, "failed to open tcp port")
+	}
+
+	serverOpts := []grpc.ServerOption{}
+
+	g.srv = grpc.NewServer(serverOpts...)
+
+	pbhighscore.RegisterGameServer(g.srv, g)
+
+	log.Info().Str("address", g.address).Msg("starting gRPC server for m-highscore microservice")
+
+	err = g.srv.Serve(lis)
+	if err != nil {
+		return errors.Wrap(err, "failed to start gRPC server for m-highscore microservice")
+	}
+	return nil
+}
